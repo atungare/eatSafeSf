fbb48424e4132994ce1af79a788f1cb485598578
Ajay Tungare <ajay.tungare@gmail.com>
2014-07-08 10:38:22 -0700

    fix typo

--- routes/checkins.js
+++ routes/checkins.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var twilio = require('helpers/twilioclient.js');
+var twilio = require('helpers/twilioClient.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
