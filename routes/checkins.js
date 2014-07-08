000cbe23f8887e6fb3bde7f96a7e40c2553da1fe
Ajay Tungare <ajay.tungare@gmail.com>
2014-07-08 10:47:13 -0700

    test twilio/foursquare link

--- routes/checkins.js
+++ routes/checkins.js
@@ -4,7 +4,20 @@ var twilio = require('../helpers/twilioClient.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  twilio.sendMessage('2019197623', 'server side message');
+  res.end();
+});
+
+router.post('/', function(req, res) {
+  var checkin = JSON.parse(req.body);
+
+  var userId = checkin.user.id;
+
+  var venue = checkin.venue;
+
+  twilio.sendMessage('2019197623', 'foursquare post - server side checkin message');
+
+  console.log(checkin, userId, venue);
+  
   res.end();
 });
 
